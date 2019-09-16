003f210acba21908e413f7ada8163347803eea66
Konstantinos-KK <54959535+Konstantinos-KK@users.noreply.github.com>
2019-09-16 14:56:17 -0400

    Update analoginput.ino

--- analoginput.ino
+++ analoginput.ino
@@ -7,7 +7,7 @@
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-int sensorPin = A5;    // select the input pin for the potentiometer
+int sensorPin = A0;    // select the input pin for the potentiometer
 float sensorValue = 0;  // variable to store the value coming from the sensor
 float a = 0;
 
